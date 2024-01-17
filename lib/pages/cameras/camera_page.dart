8c61b9f7fa46271298b76f363a17a1646f2e916d
sikigasa <km3t1114@yahoo.co.jp>
2024-01-18 02:32:49 +0900

    add: file

--- lib/pages/cameras/camera_page.dart
+++ lib/pages/cameras/camera_page.dart
@@ -1,143 +1,140 @@
-// import 'dart:async';
+import 'dart:async';
+import 'dart:io';
 
-// import 'package:camera/camera.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-// import 'package:hackathon_vol13/utils/camera.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
-// List<CameraDescription> cameras = [];
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
 
-// class CameraPage extends StatefulWidget {
-//   const CameraPage({Key? key}) : super(key: key);
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
 
-//   @override
-//   State<CameraPage> createState() => _CameraState();
-// }
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
 
-// class _CameraState extends State<CameraPage> {
-//   late CameraController _controller;
-//   final TextRecognizer _textRecognizer =
-//       TextRecognizer(script: TextRecognitionScript.japanese);
-//   bool isReady = false;
-//   bool skipScanning = false;
-//   bool isScanned = false;
-//   RecognizedText? _recognizedText;
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _setup();
-//   }
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
 
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     _textRecognizer.close();
-//     super.dispose();
-//   }
+  final CameraDescription camera;
 
-//   _processImage(CameraImage availableImage) async {
-//     if (!mounted || skipScanning) return;
-//     setState(() {
-//       skipScanning = true;
-//     });
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
 
-//     final inputImage = convert(
-//       camera: cameras[0],
-//       cameraImage: availableImage,
-//     );
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
-//     _recognizedText = await _textRecognizer.processImage(inputImage);
-//     if (!mounted) return;
-//     setState(() {
-//       skipScanning = false;
-//     });
-//     if (_recognizedText != null && _recognizedText!.text.isNotEmpty) {
-//       _controller.stopImageStream();
-//       setState(() {
-//         isScanned = true;
-//       });
-//     }
-//   }
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
 
-//   Future<void> _setup() async {
-//     cameras = await availableCameras();
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
 
-//     _controller = CameraController(cameras[0], ResolutionPreset.max);
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
 
-//     await _controller.initialize().catchError((Object e) {
-//       if (e is CameraException) {
-//         switch (e.code) {
-//           case 'CameraAccessDenied':
-//             print('User denied camera access.');
-//             break;
-//           default:
-//             print('Handle other errors.');
-//             break;
-//         }
-//       }
-//     });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until the
+      // controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
 
-//     if (!mounted) {
-//       return;
-//     }
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
+            final image = await _controller.takePicture();
 
-//     setState(() {
-//       isReady = true;
-//     });
+            if (!mounted) return;
 
-//     _controller.startImageStream(_processImage);
-//   }
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final isLoading = !isReady || !_controller.value.isInitialized;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('テキスト読み取り画面'),
-//       ),
-//       body: Column(
-//           children: isLoading
-//               ? [const Center(child: CircularProgressIndicator())]
-//               : [
-//                   Padding(
-//                       padding: const EdgeInsets.all(20),
-//                       child: AspectRatio(
-//                         aspectRatio: 12 / 9,
-//                         child: Stack(
-//                           children: [
-//                             ClipRect(
-//                               child: Transform.scale(
-//                                 scale: _controller.value.aspectRatio * 12 / 9,
-//                                 child: Center(
-//                                   child: CameraPreview(_controller),
-//                                 ),
-//                               ),
-//                             ),
-//                           ],
-//                         ),
-//                       )),
-//                   isScanned
-//                       ? ElevatedButton(
-//                           child: const Text('再度読み取る'),
-//                           onPressed: () {
-//                             setState(() {
-//                               isScanned = false;
-//                               _recognizedText = null;
-//                             });
-//                             _controller.startImageStream(_processImage);
-//                           },
-//                         )
-//                       : const Text('読み込み中'),
-//                   Expanded(
-//                     flex: 1,
-//                     child: SingleChildScrollView(
-//                       padding: const EdgeInsets.all(20),
-//                       child: Text(
-//                           _recognizedText != null ? _recognizedText!.text : ''),
-//                     ),
-//                   )
-//                 ]),
-//     );
-//   }
-// }
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
