6ea4fe7eb8434528e7a729ad63692909f604c4a8
sikigasa <km3t1114@yahoo.co.jp>
2024-01-19 00:29:16 +0900

    add: nav button

--- lib/pages/home/nav_button_icons.dart
+++ lib/pages/home/nav_button_icons.dart
@@ -17,6 +17,14 @@ class ButtonNavWithIcons extends StatefulWidget {
 
 class _ButtonNavWithIconsState extends State<ButtonNavWithIcons> {
   List<SMIBool> riveIconInputs = [];
+  List<StateMachineController?> controllers = [];
+  int selectedNavIndex = 0;
+  List<String> pages = [
+    'Chat',
+    'Search',
+    'Home',
+    'Timer',
+  ];
 
   void animateIcon(int index) {
     riveIconInputs[index].change(true);
@@ -30,21 +38,26 @@ class _ButtonNavWithIconsState extends State<ButtonNavWithIcons> {
         StateMachineController.fromArtboard(artboard, stateMachineName);
     if (controller != null) {
       artboard.addController(controller);
-      riveIconInputs.add(controller.findInput<bool>('active') as SMIBool);
-    } else {
-      controller =
-          StateMachineController.fromArtboard(artboard, 'TIME_Interactivity');
-      artboard.addController(controller!);
+      controllers.add(controller);
       riveIconInputs.add(controller.findInput<bool>('active') as SMIBool);
     }
   }
 
   @override
+  void dispose() {
+    for (var controller in controllers) {
+      controller?.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(child: Text(pages[selectedNavIndex])),
       bottomNavigationBar: SafeArea(
         child: Container(
-          height: 56,
+          height: 66,
           padding: EdgeInsets.all(12),
           margin: EdgeInsets.symmetric(horizontal: 24), //横幅
           decoration: BoxDecoration(
@@ -68,24 +81,61 @@ class _ButtonNavWithIconsState extends State<ButtonNavWithIcons> {
                 return GestureDetector(
                   onTap: () {
                     animateIcon(index);
+                    setState(() {
+                      selectedNavIndex = index;
+                    });
                   },
-                  child: SizedBox(
-                    width: 36,
-                    height: 36,
-                    child: RiveAnimation.asset(
-                      riveIcon.src,
-                      artboard: riveIcon.artboard,
-                      onInit: (artboard) {
-                        riveOnInit(artboard,
-                            stateMachineName: riveIcon.stateMachineName);
-                      },
-                    ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      AnimatedBar(
+                        isActive: selectedNavIndex == index,
+                      ),
+                      SizedBox(
+                        width: 36,
+                        height: 36,
+                        child: Opacity(
+                          opacity: selectedNavIndex == index ? 1 : 0.5,
+                          child: RiveAnimation.asset(
+                            riveIcon.src,
+                            artboard: riveIcon.artboard,
+                            onInit: (artboard) {
+                              riveOnInit(artboard,
+                                  stateMachineName: riveIcon.stateMachineName);
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
+      margin: EdgeInsets.only(bottom: 2),
+      height: 4,
+      width: isActive ? 20 : 0,
+      decoration: BoxDecoration(
+        color: Color(0xFF17203A),
+        borderRadius: BorderRadius.all(Radius.circular(6)),
       ),
     );
   }
