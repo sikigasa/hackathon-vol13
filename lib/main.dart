75c5fecf3d165290ce5ee7bc960d2ee8e297621b
sikigasa <km3t1114@yahoo.co.jp>
2024-01-20 15:20:24 +0900

    fix: import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:hackathon_vol13/pages/home/nav_button_icons.dart';
 
 Future<void> main() async {
