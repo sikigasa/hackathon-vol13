26ce805ba586b7418ca1069d6c65b121866527b9
sikigasa <km3t1114@yahoo.co.jp>
2024-01-21 02:55:06 +0900

    fix: comment out

--- lib/pages/input/input_page.dart
+++ lib/pages/input/input_page.dart
@@ -1,48 +1,17 @@
-import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:hackathon_vol13/pages/cameras/camera_page.dart';
 
 class TakePictureScreen extends StatefulWidget {
   const TakePictureScreen({
     super.key,
-    required this.camera,
   });
-
-  final CameraDescription camera;
 
   @override
   TakePictureScreenState createState() => TakePictureScreenState();
 }
 
 class TakePictureScreenState extends State<TakePictureScreen> {
-  // late CameraController _controller;
-  // late Future<void> _initializeControllerFuture;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // To display the current output from the Camera,
-  //   // create a CameraController.
-  //   _controller = CameraController(
-  //     // Get a specific camera from the list of available cameras.
-  //     widget.camera,
-  //     // Define the resolution to use.
-  //     ResolutionPreset.medium,
-  //   );
-
-  //   // Next, initialize the controller. This returns a Future.
-  //   _initializeControllerFuture = _controller.initialize();
-  // }
-
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    // _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,9 +21,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => CameraPage(
-                  // camera: widget.camera,
-                  ),
+              builder: (context) => CameraPage(),
             ),
           );
         },
@@ -137,19 +104,19 @@ class _InputFormState extends State<InputForm> {
   }
 }
 
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
+// // A widget that displays the picture taken by the user.
+// class DisplayPictureScreen extends StatelessWidget {
+//   final String imagePath;
 
-  const DisplayPictureScreen({super.key, required this.imagePath});
+//   const DisplayPictureScreen({super.key, required this.imagePath});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Display the Picture')),
+//       // The image is stored as a file on the device. Use the `Image.file`
+//       // constructor with the given path to display the image.
+//       body: Image.file(File(imagePath)),
+//     );
+//   }
+// }
