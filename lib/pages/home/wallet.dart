e3c422fcd521fb98b9e42a01ffd8ca1bce4ae244
sikigasa <km3t1114@yahoo.co.jp>
2024-01-20 15:20:12 +0900

    add: card

--- lib/pages/home/wallet.dart
+++ lib/pages/home/wallet.dart
@@ -6,8 +6,66 @@ class Wallet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Wallet'),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Text('残高'),
+
+          Center(
+            child: Card(
+              color: Color.fromARGB(255, 28, 105, 135),
+              child: InkWell(
+                splashColor: Colors.greenAccent.withAlpha(10),
+                highlightColor: Colors.transparent,
+                onTap: () {
+                  // タップ時の処理
+                },
+                child: const SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Center(
+                    child: Text('0' '円', style: TextStyle(fontSize: 30)),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Expanded(
+          //   child: WalletCard(),
+          // )
+        ],
+      ),
+    );
+  }
+}
+
+class WalletCard extends StatelessWidget {
+  const WalletCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        // alignment: AlignmentDirectional.topEnd,
+        children: [
+          // Padding(
+          //   padding: const EdgeInsets.only(right: 10),
+          // child: GestureDetector(
+          // onTap: () {
+          //     setState(() {
+          //         _isBookmarked = !_isBookmarked;
+          //     });
+          // },
+          // child: Icon(
+          // _isBookmarked
+          // ? Icons.bookmark_sharp
+          // : Icons.bookmark_outline_sharp,
+          // color: Colors.green,
+          // size: 28,
+          // ),
+          // ),
+          // ),
+        ],
       ),
     );
   }
