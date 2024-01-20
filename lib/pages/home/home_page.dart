5740f2c2f0d9999c56c38b6ec43fb7a1394842ef
sikigasa <km3t1114@yahoo.co.jp>
2024-01-20 10:47:29 +0900

    add: tab list widget

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,71 +1,70 @@
-// import 'package:flutter/material.dart';
-// import 'package:camera/camera.dart';
-// import 'package:google_ml_kit/google_ml_kit.dart';
-// import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-// import 'package:hackathon_vol13/pages/cameras/camera_page.dart';
+import 'package:flutter/material.dart';
+import 'package:hackathon_vol13/pages/home/wallet.dart';
 
-// class TakePhotoScreen extends ConsumerWidget {
-//   const TakePhotoScreen({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final cameraControllerAsync = ref.watch(cameraControllerInitProvider);
-//     final recognizeTextNotifier = ref.watch(textRecognitionProvider.notifier);
-//     final recognizeText = ref.watch(textRecognitionProvider);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-//     return Scaffold(
-//       body: cameraControllerAsync.when(
-//         data: (data) {
-//           return Center(
-//             child: Column(
-//               children: [
-//                 // カメラ正常時
-//                 if (data != null)
-//                   Column(
-//                     children: [
-//                       CameraPreview(data),
-//                       Gap(20.h),
-//                       FilledButton(
-//                         style: ButtonStyle(
-//                           backgroundColor: MaterialStateProperty.all(
-//                             AppColorSchemes.lightColorScheme.primary,
-//                           ),
-//                         ),
-//                         onPressed: () async {
-//                           try {
-//                             await scanImage(
-//                               data,
-//                               context,
-//                               recognizeTextNotifier,
-//                             );
-//                           } 
-//                           <省略>
-//                         },
-//                         child: const Text('テキストを読み取る'),
-//                       ),
-//                       Gap(20.h),
-//                       Text(
-//                         recognizeText ?? 'ここにテキストが表示されます。',
-//                         style: const TextStyle(color: Colors.black),
-//                       ),
-//                     ],
-//                   ),
-// 		  <省略>
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController;
+  int _tabLength = 4;
 
-//   Future<void> scanImage(
-//     CameraController cameraController,
-//     BuildContext context,
-//     TextRecognition recognizeTextNotifier,
-//   ) async {
-//     // 写真を撮影する
-//     final pictureFile = await cameraController.takePicture();
-//     final file = File(pictureFile.path);
-//     // 撮影した写真を読み込む
-//     final inputImage = InputImage.fromFile(file);
-//     // TextRecognizerの初期化（scriptで日本語の読み取りを指定しています※androidは日本語指定は失敗するのでデフォルトで使用すること）
-//     final textRecognizer = TextRecognizer(script: TextRecognitionScript.japanese);
-//     // 画像から文字を読み取る（OCR処理）
-//     final recognizedText = await textRecognizer.processImage(inputImage);
-//     recognizeTextNotifier.setRecognizedText(recognizedText.text);
-//   }
-// }
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _tabLength, // タブの数
+      vsync: this, // 動作させるアニメーションの種類
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose(); // 使い終わったら破棄する
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('資金管理'),
+        bottom: TabBar(
+          isScrollable: true,
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.wallet),
+            ),
+            Tab(
+              icon: Icon(Icons.savings_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.credit_card),
+            ),
+            Tab(
+              icon: Icon(Icons.add_rounded),
+            ),
+          ],
+        ),
+      ),
+      drawerScrimColor: Color.fromARGB(219, 89, 0, 255),
+      body: TabBarView(
+        controller: _tabController,
+        children: const <Widget>[
+          Wallet(),
+          Center(
+            child: Text("It's rainy here"),
+          ),
+          Center(
+            child: Text("It's sunny here"),
+          ),
+          Text("data"),
+        ],
+      ),
+    );
+  }
+}
