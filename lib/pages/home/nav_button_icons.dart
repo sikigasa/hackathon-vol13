583063db0df2b77002a6c76a8ec7180a01e5da8b
sikigasa <km3t1114@yahoo.co.jp>
2024-05-24 10:32:25 +0900

    add: async&await setState

--- lib/pages/home/nav_button_icons.dart
+++ lib/pages/home/nav_button_icons.dart
@@ -78,8 +78,8 @@ class _ButtonNavWithIconsState extends State<ButtonNavWithIcons> {
     return Scaffold(
       body: _pageOptions[selectedNavIndex](context),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          await Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => TakePictureScreen(
@@ -87,6 +87,7 @@ class _ButtonNavWithIconsState extends State<ButtonNavWithIcons> {
               ),
             ),
           );
+          setState(() {});
         },
         backgroundColor: Colors.blue,
         child: const Icon(Icons.add_rounded),
