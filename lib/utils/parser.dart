d8e8c4a31d5c099cd9fcbc009ac78c443f47fa40
sikigasa <km3t1114@yahoo.co.jp>
2024-07-11 18:06:46 +0900

    update: parse

--- lib/utils/parser.dart
+++ lib/utils/parser.dart
@@ -1,4 +1,3 @@
-import 'package:petitparser/core.dart';
 import 'package:petitparser/petitparser.dart';
 
 // トークン定義
@@ -25,25 +24,68 @@ class TokenizerDefinition extends GrammarDefinition {
   Parser whitespace1() => whitespace().plus().flatten();
 }
 
-// トークンタイプ
-enum TokenType { number, operator, identifier, parenthesis, whitespace }
-
-// トークンクラス
-class Token {
-  final TokenType type;
-  final String value;
-
-  Token(this.type, this.value);
+class TokenizerDefinition2 extends GrammarDefinition {
+  const TokenizerDefinition2();
 
   @override
-  String toString() => 'Token(type: $type, value: $value)';
+  Parser start() => ref0(token); //.star();
+
+  Parser token() => ref0(backslashNumber);
+
+  Parser backslashNumber() =>
+      (char('¥') & ref0(number) & ref0(whitespace1) | char("円").optional())
+          .map((values) {
+        final numberString = values[1].replaceFirst(RegExp(r'^0+'), '');
+        return numberString;
+      });
+
+  Parser number() => digit().plus().flatten();
+
+  Parser whitespace1() => whitespace().plus().flatten();
 }
+
+// トークンタイプ
+// enum TokenType { number, operator, identifier, parenthesis, whitespace }
+
+// トークンクラス
+// class Token {
+//   final TokenType type;
+//   final String value;
+
+//   Token(this.type, this.value);
+
+//   @override
+//   String toString() => 'Token(type: $type, value: $value)';
+// }
 
 parser(String input) {
   const tokenizer = TokenizerDefinition();
   final parser = tokenizer.build();
   final result = parser.parse(input);
 
+  // パーサのテスト
+  switch (result) {
+    case Success(value: final value):
+      print('解析成功: $value');
+    case Failure(
+        message: final message,
+        position: final position,
+        buffer: final buffer
+      ):
+      print('解析失敗: $message');
+      print('位置: $position');
+      print('バッファ: $buffer');
+    default:
+      print('解析失敗');
+  }
+  print(result.value);
+  String res = result.value.toString();
+  return res;
+}
+
+simpleParser(String input) {
+  final parser = const TokenizerDefinition2().build();
+  final result = parser.parse('¥124円 45we6');
   // パーサのテスト
   switch (result) {
     case Success(value: final value):
