9bbb1c549ef77a7e522b08d29ea6bf3d1ccb41dc
sikigasa <km3t1114@yahoo.co.jp>
2024-12-16 11:24:14 +0900

    [WIP]add: image to text

---
+++ lib/utils/geminiAPI.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+final model = GenerativeModel(
+  model: 'AIzaSyCpuFcemhcEw9PVcCm5mf6cho_79xecUBo',
+  apiKey: "apiKey",
+);
+
+Future<DataPart> fileToPart(String mimeType, String path) async {
+  return DataPart(mimeType, await File(path).readAsBytes());
+}
+
+final prompt = 'This image is receipt. Please convert it to text.';
+itoTextWithGemini(InputImage image) async {
+  final image = await fileToPart('image/jpeg', 'resources/jetpack.jpg');
+
+  final response = await model.generateContent([
+    Content.multi([TextPart(prompt), image])
+  ]);
+  print(response.text);
+}
