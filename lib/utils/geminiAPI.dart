6e5c79869d659ffe35b0db97c6c6fc65d1c7dc3e
sikigasa <km3t1114@yahoo.co.jp>
2025-01-26 16:53:04 +0900

    fix: error response

--- lib/utils/geminiAPI.dart
+++ lib/utils/geminiAPI.dart
@@ -12,6 +12,7 @@ Future<DataPart> fileToPart(String mimeType, String path) async {
 
 final prompt =
     'この写真に移っている物がレシートだった場合、支払いをした合計金額をの数字のみを出力してください。2枚以上存在する場合、それぞれの合計金額を足した数字のみを出力して下さい。レシートでなく、金額が書かれていた場合金額の数値のみを出力して下さい。それ以外だった場合-1を出力してください。';
+
 itoTextWithGemini(CameraImage cameraImage) async {
   // Convert CameraImage to DataPart
   final rgbImage = ImageUtils.convertCameraImage(cameraImage);
@@ -19,6 +20,7 @@ itoTextWithGemini(CameraImage cameraImage) async {
       rgbImage.getBytes(), cameraImage.width, cameraImage.height);
   // final jpegBytes = await convertCameraImageToJpeg(cameraImage);
   final image = DataPart('image/jpeg', jpegBytes);
+  var apiKey = prefs?.getString('GEMINI_API_KEY') ?? '';
 
   // debug save jpeg
   // saveJpeg(jpegBytes);
@@ -26,10 +28,14 @@ itoTextWithGemini(CameraImage cameraImage) async {
   prefs = await SharedPreferences.getInstance();
   final model = GenerativeModel(
     model: 'gemini-1.5-flash',
-    apiKey: prefs?.getString('GEMINI_API_KEY') ?? '',
+    apiKey: apiKey,
   );
-  final response = await model.generateContent([
-    Content.multi([TextPart(prompt), image])
-  ]);
-  return response.text;
+  try {
+    final response = await model.generateContent([
+      Content.multi([TextPart(prompt), image])
+    ]);
+    return response.text;
+  } catch (e) {
+    return 'エラーが発生しました';
+  }
 }
