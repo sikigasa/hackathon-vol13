66b4a1746766e43d912c274e52c0801bf70d4087
sikigasa <km3t1114@yahoo.co.jp>
2025-02-25 12:17:26 +0900

    add: page

---
+++ lib/pages/home/add_tab_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddTabPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Tab'),
+      ),
+      body: const Center(
+        child: Text('This is the Add Tab Page'),
+      ),
+    );
+  }
+}
