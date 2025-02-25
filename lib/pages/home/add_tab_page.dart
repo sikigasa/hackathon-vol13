7b2d76062ab3f1bcd4566a313ba7e5633c422fc4
sikigasa <km3t1114@yahoo.co.jp>
2025-02-25 19:11:56 +0900

    update: widget

--- lib/pages/home/add_tab_page.dart
+++ lib/pages/home/add_tab_page.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 class AddTabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Tab'),
-      ),
-      body: const Center(
-        child: Text('This is the Add Tab Page'),
+    return Form(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text(
+              "タブを追加する",
+              textAlign: TextAlign.left,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'タブ名',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('追加'),
+            ),
+          ],
+        ),
       ),
     );
   }
